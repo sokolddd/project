3aa235dd52dc6d3697f23753b7b6c7da0289c608
sokolddd <dasha.sokol.04@mail.ru>
2024-05-07 09:10:11 +0300

    first commit

---
+++ Grееtings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Greetings;
+{
+class Greeter
+{
+    static void Main(string[] args)
+    {
+        string myName;
+        Console.WriteLine("Please enter your name");
+        myName = Console.ReadLine();
+        Console.WriteLine("Hello, {0}", myName);
+    }
+}
+}
